18a9bfff7a17e3a70ed36e9ba16ccfaa45ce9ec9
Vojta Janousek <janousek315@gmail.com>
2019-07-22 19:21:38 +0200

    more Vue JS

---
+++ Vue_instance/list_rendering.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    users: [
+    {
+      id: 1,
+      name: 'alice',
+      profession: 'developer'
+    },
+    {
+      id: 2,
+      name: 'bob',
+      profession: 'plumber'
+    },
+    {
+      id: 3,
+      name: 'batman',
+      profession: 'manager'
+    },
+    {
+      id: 4,
+      name: 'robin',
+      profession: 'designer'
+    }]
+  }
+})
