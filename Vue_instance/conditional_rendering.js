ff108e5316a43596cafd4c8f9a4f293a8a10bb51
Vojta Janousek <janousek315@gmail.com>
2019-07-22 16:59:30 +0200

    Vue JS tested

---
+++ Vue_instance/conditional_rendering.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    auth: true,
+    product: 'sunglasses',
+    quantity: 150,
+    sale: true
+
+  }
+})
---
+++ Vue_instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "hello world",
+        value: 5,
+        imgSrc: "https://i.ytimg.com/vi/libKVRa01L8/maxresdefault.jpg",
+        link: "https://vuejs.org"
+    }
+})
